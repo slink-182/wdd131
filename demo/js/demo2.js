bca1f968e416c490159eabc399ab59df8435642d
slink-182 <slinkee182@gmail.com>
2025-06-21 23:39:40 -0600

    did most of the assignment for recipes

---
+++ demo/js/demo2.js
@@ -0,0 +1,18 @@
+// review javascript
+
+// potential to change
+let x = 15;
+let y = 10;
+let z = 15;
+
+// unchanging variables
+const a = 10;
+const b = 11;
+const c = 12;
+
+let name = "Slinkee";
+
+//console.log("Hello, " + name);
+
+console.log(`Hello, my name is ${name}`);
+
